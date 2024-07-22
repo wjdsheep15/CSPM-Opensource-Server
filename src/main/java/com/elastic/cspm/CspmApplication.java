cc584b937fe222d69840f1751637577de6e7b24b
luda412 <201810791@sangmyung.kr>
2024-07-22 13:26:37 +0900

    [CHORE]-Project Settings

---
+++ src/main/java/com/elastic/cspm/CspmApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.cspm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastic/cspm/CspmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.cspm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
