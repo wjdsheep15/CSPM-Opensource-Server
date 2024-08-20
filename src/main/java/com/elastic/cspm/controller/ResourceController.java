26f4f18ab8374fbeccf53492a4754764e0841934
lusate <rayrayr@naver.com>
2024-08-20 11:22:31 +0900

    test

--- src/main/java/com/elastic/cspm/controller/ResourceController.java
+++ src/main/java/com/elastic/cspm/controller/ResourceController.java
@@ -8,9 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
