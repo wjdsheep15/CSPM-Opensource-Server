3dfed80fe09ffdcac2e6358a58bcc2e26912a407
wjdsheep15 <wjd15sheep@gmail.com>
2024-08-21 14:25:09 +0900

    fix: feat refreshToken

--- src/main/java/com/elastic/cspm/jwt/JWTUtil.java
+++ src/main/java/com/elastic/cspm/jwt/JWTUtil.java
@@ -30,49 +30,25 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
     }
 
+    public String getCategory(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+    }
+
     public Boolean isExpired(String token) {
-        try {
-            // JWT 파싱 및 검증
-            Jws<Claims> claimsJws = Jwts.parser()
-                    .verifyWith(secretKey)
-                    .build()
-                    .parseSignedClaims(token);
 
-            // 만료 시간 확인
-            Date expiration = claimsJws.getPayload().getExpiration();
-            return expiration.before(new Date()); // 만료 여부 확인
-        } catch (ExpiredJwtException e) {
-            // 만료된 토큰일 경우
-            return true; // 만료된 경우 true 반환
-        } catch (JwtException | IllegalArgumentException e) {
-            // 다른 오류 처리 (형식 오류 등)
-            throw new RuntimeException("Invalid JWT token");
-        }
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+
     }
 
-    public String createJwt(String username, String role, Long expiredMs) {
-
-        Date now = new Date(System.currentTimeMillis());
-        Date expiryDate = new Date(now.getTime() + expiredMs);
+    public String createJwt(String category,String username, String role, Long expiredMs) {
 
         return Jwts.builder()
+                .claim("category", category)
                 .claim("username", username)
                 .claim("role", role)
-                .issuedAt(now)
-                .expiration(expiryDate)
-                .signWith(secretKey)
-                .compact();
-    }
-
-    public String createRefreshToken(String username, String role, Long expiredMs) {
-        Date now = new Date(System.currentTimeMillis());
-        Date expiryDate = new Date(now.getTime() + expiredMs);
-
-        return Jwts.builder()
-                .claim("username", username)
-                .claim("role", role)
-                .issuedAt(now)
-                .expiration(expiryDate)
+                .issuedAt( new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
     }
