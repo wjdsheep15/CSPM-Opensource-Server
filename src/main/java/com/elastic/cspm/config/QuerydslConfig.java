fc1db95d47f8dc40611ff65c3a6a6e8b2b4bb4b2
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-08-22 11:24:50 +0900

    setting: Querydsl 설정 - QuerydslConfig 설정 - 의존성은 초기 세팅 때 추가함

---
+++ src/main/java/com/elastic/cspm/config/QuerydslConfig.java
@@ -0,0 +1,21 @@
+package com.elastic.cspm.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
